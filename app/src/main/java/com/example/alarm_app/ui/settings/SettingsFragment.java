f67c0091500496983147013fc66bd012acf366d9
kkwol <kkwolny@vp.pl>
2020-08-27 19:36:24 +0200

    Added basic view of settings

--- app/src/main/java/com/example/alarm_app/ui/settings/SettingsFragment.java
+++ app/src/main/java/com/example/alarm_app/ui/settings/SettingsFragment.java
@@ -1,38 +1,16 @@
 package com.example.alarm_app.ui.settings;
 
-import androidx.lifecycle.ViewModelProviders;
-
 import android.os.Bundle;
-
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.fragment.app.Fragment;
-
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
 
 import com.example.alarm_app.R;
 
-public class SettingsFragment extends Fragment {
+import androidx.preference.PreferenceFragmentCompat;
 
-    private SettingsViewModel mViewModel;
+public class SettingsFragment extends PreferenceFragmentCompat {
 
-    public static SettingsFragment newInstance() {
-        return new SettingsFragment();
-    }
 
     @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
-                             @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_settings, container, false);
+    public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
+        setPreferencesFromResource(R.xml.prefrences, rootKey);
     }
-
-    @Override
-    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
-        super.onActivityCreated(savedInstanceState);
-        mViewModel = ViewModelProviders.of(this).get(SettingsViewModel.class);
-        // TODO: Use the ViewModel
-    }
-
 }
